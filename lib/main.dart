b8f4259385b3196541a45b45f7374f9e697a0a93
John <john.geras93@gmail.com>
2022-10-18 22:00:55 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(StartApp());
+}
+
+class StartApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unit Converter App',
+      home: DemoPage(),
+    );
+  }
+}
+
+class DemoPage extends StatefulWidget {
+  @override
+  _DemoPageState createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<DemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return MaterialApp(
+
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height * .30,
+                  width: MediaQuery.of(context).size.width * .40,
+                  color: Colors.red,
+
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text("Hello World"),
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
